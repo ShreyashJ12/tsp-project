669b801627486096e3bb7376599037910543cb48
Yash Jadhav <shreyash2jadhav1212@gmail.com>
2021-07-20 20:33:40 +0530

    first commit

---
+++ header.js
@@ -0,0 +1,22 @@
+document.write('\
+\
+    <header class="top text-gray-600 body-font">\
+    <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">\
+    <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">\
+      <img src="images\\ecology.png">\
+      <span class="ml-3 text-3xl">TreeNation</span>\
+        </a>\
+    <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">\
+    <a href = "index.html" class="mr-5 hover:text-gray-900 text-2xl">Home</a>\
+    <a href = "contact.html" class="mr-5 hover:text-gray-900 text-2xl">Contact Us</a>\
+    </nav>\
+    <a href = "https://rzp.io/l/wwJEoH40"><button class="inline-flex items-center bg-green-500 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0 text-4xl">Donate\
+    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">\
+    <path d="M5 12h14M12 5l7 7-7 7"></path>\
+    </svg>\
+    </button>\
+    </a>\
+    </div>\
+    </header>\
+\
+');
